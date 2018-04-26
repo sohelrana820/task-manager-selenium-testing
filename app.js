1858b752c382e6baba9763a9059035a4300285ee
sohelrana820 <me.sohelrana@gmail.com>
2018-04-27 02:29:55 +0600

    Initial commit!

---
+++ app.js
@@ -0,0 +1,39 @@
+var webdriver = require('selenium-webdriver'),
+    By = webdriver.By,
+    until = webdriver.until,
+    chrome = require('selenium-webdriver/chrome');
+
+var chromeCapabilities = webdriver.Capabilities.chrome();
+
+//setting chrome options to start the browser fully maximized
+var chromeOptions = {
+    'args': ['--start-maximized', '--verbose']
+};
+chromeCapabilities.set('chromeOptions', chromeOptions);
+
+var prefs = new webdriver.logging.Preferences();
+prefs.setLevel(webdriver.logging.Type.BROWSER, webdriver.logging.Level.DEBUG);
+chromeCapabilities.setLoggingPrefs(prefs);
+//webdriver.WebDriver.logging.installConsoleHandler()
+
+var chromeDriver = new webdriver.Builder()
+    .forBrowser('chrome')
+    .withCapabilities(chromeCapabilities)
+    .build();
+
+chromeDriver.manage().logs().get(webdriver.logging.Type.BROWSER)
+    .then(function(entries) {
+        entries.forEach(function(entry) {
+            console.log('[%s] %s', entry.level.name, entry.message);
+        });
+    });
+
+var configParams = {driver: chromeDriver, By: By, until: until};
+
+var login = require("./tests/login");
+login.testRedirectToLogin(configParams);
+login.testLoginPageLinks(configParams);
+login.testLoginFailed(configParams);
+login.testLoginSuccessful(configParams);
+login.logoutNow(configParams);
+chromeDriver.quit();
---
+++ tests/login.js
@@ -0,0 +1,92 @@
+var color = require("colors");
+var domainName = 'http://localhost/task-manager';
+var logoutUrl = 'http://localhost/task-manager/users/logout';
+var email = 'admin@example.com';
+var password = '123456';
+
+module.exports = {
+    testRedirectToLogin: function (config) {
+        var driver = config.driver;
+        var By = config.By;
+        var until = config.until;
+
+        console.log("================= Unauthorized User Redirection [STARTED] =============".grey);
+        driver.get(domainName);
+        driver.wait(until.titleContains('Signin - ThemeVessel Task Manager'), 10000).then(function (p1) {
+            console.log("*Checking redirection".blue);
+            console.log("\t -> Successfully redirect unauthorized users to login page".green);
+            console.log("================= Unauthorized User Redirection [ENDED] ===============".grey);
+            console.log("\n");
+        });
+    },
+    testLoginPageLinks: function (config) {
+        var driver = config.driver;
+        var By = config.By;
+        var until = config.until;
+
+        driver.get(domainName);
+        driver.findElement(By.linkText('click here to create account')).click();
+        driver.wait(until.titleContains('Create Account - ThemeVessel Task Manager'), 10000).then(function (p1) {
+            console.log("================= Login Page Links [STARTED] =============".grey);
+            console.log("*Checking links".blue);
+            console.log("\t -> Signup/Create Account Link Good!".green);
+        });
+
+        driver.get(domainName);
+        driver.findElement(By.linkText('Forgot password')).click();
+        driver.wait(until.titleContains('Forgot Password - ThemeVessel Task Manager'), 10000).then(function (p1) {
+            console.log("\t -> Forgot Password Link Good!".green);
+            console.log("================= Login Page Links [ENDED] ===============".grey);
+            console.log("\n");
+        });
+    },
+    testLoginFailed: function (config) {
+        var driver = config.driver;
+        var By = config.By;
+        var until = config.until;
+
+        driver.get(logoutUrl);
+        driver.findElement(By.name('username')).sendKeys(email);
+        driver.findElement(By.name('password')).sendKeys('');
+        driver.findElement(By.className('btn btn-primary login-button')).click();
+        driver.wait(driver.findElement(By.className('text-center alert alert-danger')), 10000).then(function (p1) {
+            console.log("================= Failed Login [STARTED] =============".grey);
+            console.log("*Checking failed Login".blue);
+            console.log("\t -> Failed login & displaying error flash message".green);
+            console.log("================= Failed Login [STARTED] =============".grey);
+            console.log("\n");
+        });
+    },
+    testLoginSuccessful: function (config) {
+        var driver = config.driver;
+        var By = config.By;
+        var until = config.until;
+
+        //Checking successful login
+        driver.get(domainName);
+        driver.findElement(By.name('username')).sendKeys(email);
+        driver.findElement(By.name('password')).sendKeys(password);
+        driver.findElement(By.className('btn btn-primary login-button')).click();
+        driver.wait(until.titleContains('My Dashboard - ThemeVessel Task Manager'), 10000).then(function (p1) {
+            console.log("================= Successful Login [STARTED] =============".grey);
+            console.log("*Checking successful Login".blue);
+            console.log("\t -> Successful login & redirect to dashboard".green);
+            console.log("================= Successful Login [ENDED] ===============".grey);
+            console.log("\n");
+        });
+    },
+    logoutNow: function (config) {
+        var driver = config.driver;
+        var By = config.By;
+        var until = config.until;
+
+        driver.get(logoutUrl);
+        driver.wait(until.titleContains('Signin - ThemeVessel Task Manager'), 10000).then(function (p1) {
+            console.log("================= Successful Logout [STARTED] =============".grey);
+            console.log("*Checking logout".blue);
+            console.log("\t -> Successfully loggedout and redirect to login page".green);
+            console.log("================= Successful Logout [ENDED] ===============".grey);
+            console.log("\n");
+        });
+    }
+};
